a59a85f00ed038245088631e54aeee294a74f8ed
Pawel Wozniak <pawek13.05.98@gmail.com>
2024-06-23 19:25:02 +0200

    5.3

--- main.js
+++ main.js
@@ -69,12 +69,28 @@ function displayData(data) {
 }
 
 // Load data from sessionStorage on page load
-document.addEventListener('DOMContentLoaded', function() {
-    const data = JSON.parse(sessionStorage.getItem('csvData'));
-    if (data) {
-        displayData(data);
-        console.log('Data loaded from sessionStorage on page load:', data);
-    } else {
-        console.log('No data in sessionStorage');
-    }
+document.getElementById('uploadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Form submitted');
+    const fileInput = document.getElementById('csvFile');
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        console.log('File loaded');
+        const csvData = e.target.result;
+        const parsedData = Papa.parse(csvData, {
+            header: true,
+            dynamicTyping: true,
+            skipEmptyLines: true
+        });
+        console.log('Parsed data:', parsedData);
+
+        sessionStorage.setItem('csvData', JSON.stringify(parsedData.data));
+        console.log('Data saved to sessionStorage');
+
+        window.location.href = 'analiza.html';
+    };
+
+    reader.readAsText(file);
 });
